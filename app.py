1228b07c00da4eb33e8a14feb6b030ac316a47ef
nsbt <55523468+nsbt@users.noreply.github.com>
2023-09-20 09:07:16 +0000

    add rekognition api call

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
+import boto3
+import base64
+import logging
+import traceback,sys
 from chalice import Chalice
 
 app = Chalice(app_name='rekognition-chalice')
-
+app.debug = True
+ 
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 @app.route('/rekognition', methods=['POST'], content_types=['application/octet-stream'], cors=True)
 def rekognition():
-    return '猫 69%, ごみ袋 31%'
+    try:
+        logger.info('Invoke Rekognition')
+
+        res = boto3.client(service_name='rekognition', region_name='ap-northeast-1').detect_labels(
+            Image = {
+                'Bytes': base64.b64decode(app.current_request.raw_body.split(b'base64,')[1]),
+            },
+            MaxLabels=5,
+            MinConfidence=10,
+        )
+ 
+        return ','.join([f'{label["Name"]} {label["Confidence"]:.2f}%,' for label in res['Labels']])
+
+    except Exception as e:
+        tb = sys.exc_info()[2]
+        return 'error:{0}'.format(e.with_traceback(tb))
